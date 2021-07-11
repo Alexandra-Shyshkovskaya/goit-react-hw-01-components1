fe744965cba7efce1be37d854dd0d7ebf2b910c1
Alexandra-Shyshkovskaya <74681434+Alexandra-Shyshkovskaya@users.noreply.github.com>
2021-07-11 15:07:36 +0300

    попытка №2, добавляет файлы

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Statistics.module.css";
+
+const Statistics = ({ title, stats }) => (
+  <section className={styles.statistics}>
+    {title && <h2 className={styles.title}>{title}</h2>}
+
+    <ul className={styles.stat_list}>
+      {stats.map(({ id, label, percentage }) => (
+        <li
+          key={id}
+          className={styles.item}
+          style={{
+            backgroundColor: `rgb(${getRandom(0, 255)}, ${getRandom(
+              0,
+              255
+            )}, ${getRandom(0, 255)})`,
+          }}
+        >
+          <span className={styles.label}>{label}</span>
+          <span className={styles.percentage}>{percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Statistics.defaultProps = {
+  title: "",
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+function getRandom(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
+}
+
+export default Statistics;
---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ items }) => (
+  <table className={styles.transaction_history}>
+    <thead>
+      <tr>
+        <th className={styles.title}>Type</th>
+        <th className={styles.title}>Amount</th>
+        <th className={styles.title}>Currency</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {items.map((item) => (
+        <tr className={styles.row} key={item.id}>
+          <td className={styles.td}>{item.type}</td>
+          <td className={styles.td}>{item.amount}</td>
+          <td className={styles.td}>{item.currency}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default TransactionHistory;
